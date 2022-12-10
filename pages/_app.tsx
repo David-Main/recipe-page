07eec145ad4bf72a6e90e9a330fadfe2916d59f1
David-Main <work.davidmainoo@gmail.com>
2022-12-10 18:18:34 +0000

    Add custom font and configure them to be used with tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,44 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Montserrat } from "@next/font/google";
+import { Playfair_Display } from "@next/font/google";
+
+/* 
+  Configure fonts
+*/
+
+const montserrat = Montserrat({
+	subsets: ["latin"],
+	variable: "--font-montserrat",
+});
+const play_Display = Playfair_Display({
+	subsets: ["latin"],
+	variable: "--font-playfair",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<style jsx global>
+				{`
+					@layer base {
+						body {
+							font-family: ${montserrat.style.fontFamily};
+						}
+
+						h1,
+						h2,
+						h3,
+						h4,
+						h5,
+						h6 {
+							font-family: ${play_Display.style.fontFamily};
+						}
+					}
+				`}
+			</style>
+			<Component {...pageProps} />
+		</>
+	);
 }
